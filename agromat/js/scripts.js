68892b9ab5372e1a3ec523e5a2a4682e7c98c179
vitaliyvdv <vvvd@mail.ru>
2016-11-14 01:01:28 +0200

    agromat

---
+++ agromat/js/scripts.js
@@ -0,0 +1,27 @@
+head.ready(function() {
+
+    // lazy load для изображений
+    /*
+        $('.lazyload').lazyLoadXT({
+            blankImage: 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'
+        });*/
+
+    // no transition
+
+    var noTransition = function() {
+        setTimeout(function() {
+            $('body').removeClass('no-transition');
+        }, 0);
+    };
+
+
+    $.scrollIt({
+        upKey: 38, // key code to navigate to the next section
+        downKey: 40, // key code to navigate to the previous section
+        easing: 'linear', // the easing function for animation
+        scrollTime: 600, // how long (in ms) the animation takes
+        activeClass: 'active', // class given to the active nav element
+        onPageChange: null, // function(pageIndex) that is called when page is changed
+        topOffset: -64 // offste (in px) for fixed top navigation
+    });
+});
